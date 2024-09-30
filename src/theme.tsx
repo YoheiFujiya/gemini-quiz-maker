fdbe392d59cd28b4e7f3d566064e3ecc5e3bf7dd
YoheiFujiya <fujiya.yohei@opst.co.jp>
2024-09-30 09:14:15 +0900

    Update file paths and remove unused font link in index.html

--- src/theme.tsx
+++ src/theme.tsx
@@ -1,27 +1,10 @@
 import { createTheme } from "@mui/material/styles";
 
 const theme = createTheme({
-  // palette: {
-  //   mode: "dark",
-  // },
   components: {
     MuiTypography: {
       defaultProps: {
         gutterBottom: true,
-      },
-      // styleOverrides: {
-      //   h1: {
-      //     fontSize: "3.5rem",
-      //     fontWeight: 500,
-      //   },
-      // },
-      styleOverrides: {
-        h4: {
-          fontFamily: "DotGothic16",
-        },
-        h6: {
-          fontFamily: "DotGothic16",
-        },
       },
     },
     MuiButton: {
@@ -29,9 +12,7 @@ const theme = createTheme({
         variant: "contained",
         color: "primary",
         fullWidth: true,
-      },
-      styleOverrides: {
-        root: {
+        sx: {
           marginBottom: "10px",
           height: "80px",
           fontSize: "1.5rem",
